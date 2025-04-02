e48bd94e9dfa9a88232741bdd49247eadb04f1fe
Mohammed Hosein Ali <2055295@edu.vaniercollege.qc.ca>
2025-04-02 12:02:10 -0400

    done before deadline but forgot to submit on time

--- Assets/Scripts/State/AttackState.cs
+++ Assets/Scripts/State/AttackState.cs
@@ -3,5 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class AttackState : IState
 {
+    private AIController aiController;
     
+    public StateType Type => StateType.Attack;
+
+    public AttackState(AIController aiController)
+    {
+        this.aiController = aiController;
+    }
+
+    public void Enter()
+    {
+        aiController.Animator.SetBool("isAttacking", true);
+        aiController.Agent.isStopped = true; 
+    }
+
+    public void Execute()
+    {
+        //check if the player is in range of attack
+        if(Vector3.Distance(aiController.transform.position, aiController.Player.position)
+        > aiController.AttackRange)
+        {
+            //if the player moves away, transition back to chaseState
+            aiController.StateMachine.TransitionToState(StateType.Chase);
+            return;
+        }
+    }
+
+    public void Exit()
+    {
+    aiController.Animator.SetBool("isAttacking", true);
+    aiController.Agent.isStopped = false; // Resume the AI agent movement
+    }  
 }
